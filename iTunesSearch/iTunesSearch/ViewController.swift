1e6335f7d349a92b6a7189f142be783e81cacb96
Oliver Paray <omparay@gmail.com>
2020-04-01 02:46:04 -0700

    Verified search is working
    Now I just need to:
    1. Summarize the search results
    2. Display the summary
    3. Enable the marking of favorites

--- iTunesSearch/iTunesSearch/ViewController.swift
+++ iTunesSearch/iTunesSearch/ViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2020 Oliver Paray. All rights reserved.
 //
 
+import Library
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
@@ -15,13 +16,42 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
     @IBOutlet weak var criteriaField: UITextField!
     @IBOutlet weak var resultsInstructionLabel: UILabel!
     @IBOutlet weak var resultsTableView: UITableView!
+    @IBOutlet weak var searchActivity: UIActivityIndicatorView!
+
+    //Properties
+    private var alert: UIAlertController = UIAlertController(title: "Alert", message: String.empty, preferredStyle: .alert)
+    private var results: JSON?
+    private var summarized = [String:JSON]()
+    private var hasResults: Bool {
+        get {
+            if self.results != nil {
+                return true
+            } else {
+                self.displayError(withTitle: "Error...", andMessage: "No results to display.")
+                return false
+            }
+        }
+    }
 
     //View Lifecycle
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(handleTextChanged(sender:)), name: UITextField.textDidChangeNotification, object: self.criteriaField)
     }
 
     //Datasource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return summarized.allKeys.count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return String(describing: summarized.allKeys[section])
+    }
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
     }
@@ -32,17 +62,72 @@ class ViewController: UIViewController, UITextFieldDelegate, UITableViewDelegate
     }
 
     //Delegates
-
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
+        //Mark this as a favorite...
     }
 
-    func textFieldDidEndEditing(_ textField: UITextField) {
-
+    //Notification Handlers
+    @objc func handleTextChanged(sender: NSNotification){
+        guard let field = sender.object as? UITextField, let text = field.text else { return }
+        debugPrint("Search Text: \(text)")
+        searchAnimation()
+        iTunesSearch.performSearch(withTerm: text, handler: (
+            success: {
+                (httpResponse, httpData) in
+                guard let response = httpResponse as? HTTPURLResponse, let data = httpData else {
+                    self.displayError()
+                    return
+                }
+                if response.statusCode != 200 {
+                    self.displayError(withTitle: "HTTP Error...", andMessage: "No Internet Connection?\nTyu again later...")
+                }
+                self.processResult(data)
+            },
+            failure: {
+                (httpResponse,systemError,errorMessage) in
+                guard let message = errorMessage else {
+                    self.displayError()
+                    return
+                }
+                self.displayError(withTitle: "Error...", andMessage: message)
+            })
+        )
     }
 
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        return (textField == criteriaField)
+    //Private Methods
+    private func searchAnimation(_ start: Bool=true){
+        DispatchQueue.main.async {
+            self.searchActivity.isHidden = !start
+            if start {
+                self.searchActivity.startAnimating()
+            } else {
+                self.searchActivity.stopAnimating()
+            }
+        }
     }
+
+    private func processResult(_ data:Data){
+        guard let jsonData = Parser.jsonFrom(data: data) else {
+            self.displayError()
+            return
+        }
+        self.results = jsonData
+        DispatchQueue.main.async {
+            self.searchAnimation(false)
+            //Process and display results
+        }
+    }
+
+    private func displayError(withTitle: String = "Ooops...",
+                              andMessage:String = "Something weird has happened.\nPlease try again."){
+        DispatchQueue.main.async {
+            self.alert = UIAlertController(title: withTitle, message: andMessage, preferredStyle: .alert)
+            self.alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+                self.alert.dismiss(animated: true, completion: nil)
+            }))
+            self.searchAnimation(false)
+            self.present(self.alert, animated: true, completion: nil)
+        }
+    }
+
 }
-
